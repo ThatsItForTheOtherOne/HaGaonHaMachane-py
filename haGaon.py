c41efc8a58254bd53d301e030e82adf76dcd653c
Daniel Kirsch <djkirsch326@gmail.com>
2020-05-05 16:26:36 -0400

    Personal setup

--- haGaon.py
+++ haGaon.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import json
 from urllib.request import urlopen
 import re
-
+import os.path
 
 bot = commands.Bot(command_prefix='!!', description='HaGaon HaMachane Reborn!')
 bot.remove_command('help')
@@ -17,5 +17,8 @@ async def on_ready():
         for cog in cog_list:
                 bot.load_extension(cog)
 
-token = open("token.txt", "r").read()
+if (os.path.isfile(token.txt)): 
+        token = open("token.txt", "r").read()
+else:
+        token = open("/hagaon/token.txt", "r").read()
 bot.run(token.strip())
