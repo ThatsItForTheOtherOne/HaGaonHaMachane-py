7946df5f0365106e4acef033854de91645a542f9
Daniel Joseph Kirsch <djkirsch326@gmail.com>
2020-05-14 21:42:22 -0400

    Add daf yomi to eventsToday

--- HebrewDate.py
+++ HebrewDate.py
@@ -20,13 +20,16 @@ class HebrewDate(commands.Cog):
         date = hdate.HDate(datetime.datetime.now(), hebrew=False)
         if date.is_holiday == False and 0 < date.omer_day < 50:
             eventStr = ''.join(('The parasha is ', date.parasha, 
-                                ' and it is', str(date.omer_day), ' days in the omer.'))
+                                ' and it is ', str(date.omer_day), ' days in the omer.'
+                                ' The daf yomi is ', date.daf_yomi, '.'))
         elif date.is_holiday == False:
             eventStr = ''.join(('The parasha is ', date.parasha, 
-                                ' and today is not a holiday.'))
+                                ' and today is not a holiday.'
+                                ' The daf yomi is ', date.daf_yomi, '.'))
         else:
             eventStr = ''.join(('The parasha is  ', date.parasha, 
-                                ' and today is ', date.holiday_name, '.'))
+                                ' and today is ', date.holiday_name, '.'
+                                ' The daf yomi is ', date.daf_yomi, '.'))
 
         await createEmbed(ctx, eventStr)
     
@@ -35,6 +38,7 @@ class HebrewDate(commands.Cog):
         gregorianDate = datetime.datetime(int(year), int(month), int(day))
         date = hdate.HDate(gregorianDate, hebrew=False)
         await createEmbed(ctx, date.hebrew_date)
+    
 
 
 
