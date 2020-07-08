90205ef13cc2dc1b5d369a82101f0159b2f3f5ab
Acher224 <tzipipi@protonmail.com>
2020-07-07 23:59:04 -0400

    Bump version cuz why tf not lole

--- haGaon.py
+++ haGaon.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os.path
 import sqlite3
 
-bot = commands.Bot(command_prefix="!!", description="HaGaon HaMachane Version 1.4")
+bot = commands.Bot(command_prefix="!!", description="HaGaon HaMachane Version 2.0")
 bot.remove_command("help")
 
 
