e2ade66b91c629ef8c3ecfe7193c8a38d4b7e2ac
Acher224 <tzipipi@protonmail.com>
2020-07-08 09:22:56 -0400

    Make setting location only work in DMs for user security

--- Zmanim.py
+++ Zmanim.py
@@ -14,23 +14,26 @@ class Zmanim(commands.Cog):
 
     @commands.command(name="setLocation")
     async def setLocation(self, ctx, latitude, longtiude, timezone, diaspora):
-        db = sqlite3.connect("haGaon.db")
-        cursor = db.cursor()
-        cursor.execute(
-            f"SELECT user_id FROM main WHERE user_id = {ctx.message.author.id}"
-        )
-        result = cursor.fetchone()
-        if result is None:
-            sql = "INSERT INTO main(user_id, latitude, longitude, timezone, diaspora) VALUES(?, ?, ?, ?, ?)"
-            val = (ctx.message.author.id, latitude, longtiude, timezone, diaspora)
-        elif result is not None:
-            sql = "UPDATE main SET latitude = ?, longitude  = ?, timezone = ?, diaspora = ? WHERE user_id = ?"
-            val = (latitude, longtiude, timezone, diaspora, ctx.message.author.id)
-        cursor.execute(sql, val)
-        db.commit()
-        cursor.close()
-        db.close()
-        await createEmbed(ctx, "Location Set and Saved!")
+        if ctx.channel.type is not discord.ChannelType.private:
+            await createEmbed(ctx, "This command is only for DMs!")
+        else:
+            db = sqlite3.connect("haGaon.db")
+            cursor = db.cursor()
+            cursor.execute(
+              f"SELECT user_id FROM main WHERE user_id = {ctx.message.author.id}"
+         )
+            result = cursor.fetchone()
+            if result is None:
+                sql = "INSERT INTO main(user_id, latitude, longitude, timezone, diaspora) VALUES(?, ?, ?, ?, ?)"
+                val = (ctx.message.author.id, latitude, longtiude, timezone, diaspora)
+            elif result is not None:
+                sql = "UPDATE main SET latitude = ?, longitude  = ?, timezone = ?, diaspora = ? WHERE user_id = ?"
+                val = (latitude, longtiude, timezone, diaspora, ctx.message.author.id)
+            cursor.execute(sql, val)
+            db.commit()
+            cursor.close()
+            db.close()
+            await createEmbed(ctx, "Location Set and Saved!")
 
     @commands.command(name="zmanim")
     async def getZmanim(self, ctx):
