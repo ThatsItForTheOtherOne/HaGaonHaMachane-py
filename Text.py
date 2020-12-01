e98694b5cd35d853d704490fc9ff0dcef8ebe5cc
Acher224 <tzipipi@protonmail.com>
2020-11-30 19:12:43 -0500

    Refactor text command to be better python

--- Text.py
+++ Text.py
@@ -1,13 +1,14 @@
+import aiohttp
 import discord
 from discord.ext import commands
 import json
 from urllib.request import urlopen
-from SendText import create_embed
+from sendText import create_embed
 from aiohttp import ClientSession
 from markdownify import markdownify as md
 import sqlite3
 import re
-
+from typing import List, Tuple
 
 class Text(commands.Cog):
     def __init__(self, bot):
@@ -70,52 +71,43 @@ class Text(commands.Cog):
 
     @commands.command(name="text")
     async def text_command(self, ctx, *verse):
-        verse = " ".join(verse)
-        verse = self.replace_spaces_with_underscores(verse)
-        if "-" in verse:
-            parsed_string = re.compile(r"(\S+)\s(\S+):(\d+)-(\d+)", re.IGNORECASE).match(verse).groups()
-            book = parsed_string[0]
-            chapter = parsed_string[1]
-            first_verse = int(parsed_string[2])
-            final_verse = int(parsed_string[3])
+        def parse_verse(verse: str) -> str:
+            def parse_verses(verse: str) -> Tuple[str, str]:
+                if "-" in verse:
+                    book, chapter, first, final = re.compile(r"(\S+)\s(\S+):(\d+)-(\d+)", re.IGNORECASE).match(verse).groups()
+                    return (f"{book}.{chapter}.{first}-{final}", book)
 
-            api_url = f"{self.api_url}{book}.{chapter}.{first_verse}?{self.get_translation(ctx, book)}"
-            sefaria_obj = json.load(urlopen(api_url))
-            verses_list = sefaria_obj["text"][first_verse - 1 : final_verse]
-            verse_text = " ".join(verses_list)
-            if verse_text is "":
-                await create_embed(
-                    ctx, "There is no translation for this verse (or your translation setting is invalid)."
-                )
-                return
-            await create_embed(ctx, md(verse_text))
-        elif ":" in verse:
-            parsed_string = re.compile(r"(\S+)\s(\S+):(\d+)", re.IGNORECASE).match(verse).groups()
-            book = parsed_string[0]
-            chapter = parsed_string[1]
-            verse = int(parsed_string[2])
+                elif ":" in verse:
+                    book, chapter, verse = re.compile(r"(\S+)\s(\S+):(\d+)", re.IGNORECASE).match(verse).groups()
+                    return (f"{book}.{chapter}.{verse}", book)
 
-            api_url = f"{self.api_url}{book}.{chapter}.{verse}?context=0{self.get_translation(ctx, book)}"
-            sefaria_obj = json.load(urlopen(api_url))
-            if sefaria_obj["text"] is "":
-                await create_embed(
-                    ctx, "There is no translation for this verse (or your translation setting is invalid)."
-                )
-                return
-            await create_embed(ctx, md(sefaria_obj["text"]))
+                else:
+                    book, verse = re.compile(r"(\S+)\s(\S+)", re.IGNORECASE).match(verse).groups()
+                    return (f"{book}.{verse}", book)
+
+            url, book = parse_verses(verse)
+            return f"{self.api_url}{url}?context=0{self.get_translation(ctx, book)}"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(parse_verse(self.replace_spaces_with_underscores(" ".join(verse)))) as response:
+                body = await response.text()
+
+                if response.status == 404:
+                    await create_embed(ctx, "This verse doesn't exist. Check that your input is well formed, or that the verse is available in your desired translation.")
+                    return
+
+                sefaria_obj = json.loads(body)
+
+        if isinstance(verses := sefaria_obj["text"], list):
+            verse_text = " ".join(verses)
+        elif isinstance(verse := sefaria_obj["text"], str):
+            verse_text = verse
         else:
-            parsed_string = re.compile(r"(\S+)\s(\S+)", re.IGNORECASE).match(verse).groups()
-            book = parsed_string[0]
-            verse = int(parsed_string[1])
+            typeof = type(sefaria_obj["text"]).__name__
+            await create_embed(ctx, f"The Sefaria API returned something that I don't understand ({typeof}). Please contact my administrator about this error.")
+            return
 
-            api_url = f"{self.api_url}{book}.{verse}?context=0{self.get_translation(ctx, book)}"
-            sefaria_obj = json.load(urlopen(api_url))
-            if sefaria_obj["text"] is "":
-                await create_embed(
-                    ctx, "There is no translation for this verse (or your translation setting is invalid)."
-                )
-                return
-            await create_embed(ctx, md(sefaria_obj["text"]))
+        await create_embed(ctx, md(verse_text))
 
     @commands.command(name="hebrewText")
     async def hebrew_text_command(self, ctx, *verse):
